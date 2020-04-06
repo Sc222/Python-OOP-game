b7ed7c86fd4f1dcd26a55942241c0449f10589f3
balandin228 <55559705+balandin228@users.noreply.github.com>
2020-04-06 23:02:54 +0500

    addUser test

--- tests/dbHelperTest.py
+++ tests/dbHelperTest.py
@@ -1,4 +1,5 @@
 from main.db_helper import DbHelper
+from main.player import Player
 import unittest
 
 
@@ -6,3 +7,10 @@ class DbHelperTest(unittest.TestCase):
     def setUp(self):
         self.dbHelper = DbHelper()
 
+    def test_AddUser(self):
+        self.dbHelper.RegisterUser("govno","GOVNO")
+        user: Player = self.dbHelper.GetUser("govno","GOVNO")
+        self.assertEqual(user.nickname == "govno")
+        self.assertEqual(user.password == "GOVNO")
+if __name__ == "__main__":
+  unittest.main()
