1c7e7e80a2648befe94c30672a84c8aca0cfca88
sc222 <v.a.shaternikov@gmail.com>
2020-04-06 13:11:44 +0500

    db helper class setup

---
+++ main/db_helper.py
@@ -0,0 +1,13 @@
+from main.background import Background
+from main.terrain import Terrain
+from main.monster import Monster
+from main.background_info import BackgroundInfo
+from main.terrain_info import TerrainInfo
+from main.monster_info import MonsterInfo
+from main.leaderboard_record import LeaderboardRecord
+from main.player import Player
+from main.level import Level
+
+# здесь будут методы для работы с базой данных
+class Dbhelper():
+    pass
