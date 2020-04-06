a18def9961709aea667392abd5d2865ab70aa4b4
sc222 <v.a.shaternikov@gmail.com>
2020-04-06 18:06:11 +0500

    leaderboard test

--- tests/dbHelperTest.py
+++ tests/dbHelperTest.py
@@ -5,4 +5,9 @@ import unittest
 class DbHelperTest(unittest.TestCase):
     def setUp(self):
         self.dbHelper = DbHelper()
+        self.dbHelper.ClearAllBase()
 
+    def createLeaderboard(self):
+        self.dbHelper.CreateLeaderboardRecord(0, 1, 100)
+        records = list(self.dbHelper.GetLeaderboards(0))
+        self.assertEqual(len(records), 1, "leaderboard wasn't created")
