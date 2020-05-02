05be8846e4d77e8db5a745df20332949f1dd0520
Sc222 <v.a.shaternikov@gmail.com>
2020-05-02 15:47:00 +0500

    add init, move constants in separate file

---
+++ main/gui/constants.py
@@ -0,0 +1,20 @@
+FPS = 60
+TERRAIN_SHIFT = -6
+WINDOWWIDTH = 900
+WINDOWHEIGHT = 600
+SCALE = 5
+TILECOLLIDEWIDTH = 21 * SCALE
+TILEWIDTH = 32 * SCALE  # holds the tile width and height
+TILEHEIGHT = 32 * SCALE
+TILEHEIGHT_HALF = TILEHEIGHT / 2
+TILEWIDTH_HALF = TILEWIDTH / 2
+MOUSE_IDLE_DELTA = 40  # когда разница в координатах меньше этого числа игрок стоит на месте
+PLAYER_SIZE = 24 * SCALE
+PLAYER_COLLIDE_WIDTH = 6 * SCALE
+PLAYER_COLLUDE_HEIGHT = 5 * SCALE
+MOVE_COLLIDE_RECT_OFFSET = 2  # cкорость движения по осям умножается на это число и хитбокс сдвигается
+#colors
+SKY = (0, 205, 249)
+DEBUG = (255, 0, 64)
+PL = (0, 255, 0)
+TR = (0, 0, 255)
