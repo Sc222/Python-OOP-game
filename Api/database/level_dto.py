b1be90a07645ed56ba56699ff1856008b3f5e253
Balandin228 <igorek.chelovek@gmail.com>
2020-06-23 00:08:54 +0500

    add level url

--- Api/database/level_dto.py
+++
@@ -1,8 +0,0 @@
-class LevelDto:
-
-    def __init__(self, dbLevel, backgroundsInfo, monstersInfo, terrainsInfo):
-        self.dbLevel = dbLevel
-        self.backgroundsInfo = backgroundsInfo
-        self.monstersInfo = monstersInfo
-        self.terrainsInfo = terrainsInfo
-        # чтобы получить monsters, terrains, backgrounds использовать list(dbLevel.monsters)
--- Api/database/models_DTO.py
+++ Api/database/models_DTO.py
@@ -4,3 +4,19 @@ class Leaderboard_Dto():
         self.playerId = playerId
         self.levelId = levelId
         self.score = score
+
+class Background_Dto():
+    x = 0
+    y = 0
+    imageSource = ''
+    def __init__(self,x,y,imageSource):
+        self.x = x
+        self.y = y
+        self.imageSource = imageSource
+
+class LevelDto:
+
+    def __init__(self,backgrounds, monsters, terrains):
+        self.backgrounds = backgrounds
+        self.monsters = monsters
+        self.terrains = terrains
