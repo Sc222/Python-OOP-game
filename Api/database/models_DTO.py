0f04c684f00dfe0912eb944fea2a944bb6c6606e
Sc222 <v.a.shaternikov@gmail.com>
2020-06-23 15:13:08 +0500

    gitignore update, api reformat

--- Api/database/models_DTO.py
+++ Api/database/models_DTO.py
@@ -1,22 +1,25 @@
 class Leaderboard_Dto():
-    def __init__(self,id,playerId,levelId,score):
+    def __init__(self, id, playerId, levelId, score):
         self.id = id
         self.playerId = playerId
         self.levelId = levelId
         self.score = score
 
+
 class Background_Dto():
     x = 0
     y = 0
     imageSource = ''
-    def __init__(self,x,y,imageSource):
+
+    def __init__(self, x, y, imageSource):
         self.x = x
         self.y = y
         self.imageSource = imageSource
 
+
 class LevelDto:
 
-    def __init__(self,backgrounds, monsters, terrains):
+    def __init__(self, backgrounds, monsters, terrains):
         self.backgrounds = backgrounds
         self.monsters = monsters
         self.terrains = terrains
--- Api/routes.py
+++ Api/routes.py
@@ -1,13 +1,16 @@
 from app import app, db
 from database import models, models_DTO
-from database.models_DTO import Background_Dto,LevelDto,Leaderboard_Dto
+from database.models_DTO import Background_Dto, LevelDto, Leaderboard_Dto
 from flask import jsonify
-@app.route('/leaderboard',methods=['GET'])
+
+
+@app.route('/leaderboard', methods=['GET'])
 def Leaderboard():
     return jsonify([l.serialize() for l in models.LeaderboardRecord.query.all()])
 
-@app.route('/level/<id>',methods=['GET'])
+
+@app.route('/level/<id>', methods=['GET'])
 def getLevel(id):
     dbLevel = models.Level.query.get(id)
-    backgrounds_Dto = [Background_Dto(b.x,b.y,b.info.imageSource).__dict__ for b in dbLevel.backgrounds]
-    return jsonify(LevelDto(backgrounds_Dto,[],[]).__dict__)
+    backgrounds_Dto = [Background_Dto(b.x, b.y, b.info.imageSource).__dict__ for b in dbLevel.backgrounds]
+    return jsonify(LevelDto(backgrounds_Dto, [], []).__dict__)
