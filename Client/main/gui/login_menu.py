92a893a5a92be736ba0195af6e397ca7172db848
Sc222 <v.a.shaternikov@gmail.com>
2020-06-27 06:56:12 +0500

    finish login menu

--- Client/main/gui/login_menu.py
+++ Client/main/gui/login_menu.py
@@ -1,17 +1,25 @@
 import pygame
 import thorpy
+from thorpy import Inserter
+from thorpy.elements._inserterutils._insertwriter import _InsertWriter
+from thorpy.miscgui import painterstyle, functions
 
-from main.gui import game_main, leaderboards_menu
-from main.gui.constants import FPS, GAME_NAME, MAIN_MENU_HEADER, GUI_SCALE
+from thorpy.painting.painters.imageframe import ImageButton
+from thorpy.painting.painters.painter import Painter
+
+from main.gui import game_main, leaderboards_menu, gui_utils
+from main.gui.constants import FPS, GAME_NAME, MAIN_MENU_HEADER, GUI_SCALE, MONOSPACE_FONT, TRANSPARENT, WHITE, RED
 from main.gui.gui_utils import create_button, render_text
+from main.gui.main_menu import MainMenu
 from main.gui.main_menu_utils import ScrollingBackgroundHorizontal, PlayerMenu
+from main.server_connector.server_connector import ServerConnector
 
 
 class LoginMenu:
 
     def __init__(self, res, screen, clock):
         self.is_opened = True
-        self.resources = res
+        self.res = res
         self.screen = screen
         self.clock = clock
         self.background_image = res.load_login_background()
@@ -23,20 +31,68 @@ class LoginMenu:
         button_exit_login = create_button(res.load_button_images("exit_login_menu"), self.exit_login_menu)
         button_exit_login.set_topleft((68 * GUI_SCALE, 24 * GUI_SCALE))
 
+        button_login = create_button(res.load_button_images("login"), self.login)
+        button_login.set_topleft((95 * GUI_SCALE, 146 * GUI_SCALE))
+
+        button_register = create_button(res.load_button_images("register"), self.register)
+        button_register.set_topleft((149 * GUI_SCALE, 146 * GUI_SCALE))
+
+        text_username = gui_utils.create_text("Username:", (110 * GUI_SCALE, 65 * GUI_SCALE), WHITE, 10 * GUI_SCALE)
+        text_password = gui_utils.create_text("Password:",(110 * GUI_SCALE, 102 * GUI_SCALE),WHITE,10*GUI_SCALE)
+        self.text_error = gui_utils.create_text("", (108 * GUI_SCALE, 135 * GUI_SCALE), RED, 8 * GUI_SCALE)
+
+        self.inserter_username = gui_utils.create_inserter(res)
+        inserter_username_container = thorpy.Ghost(elements=[self.inserter_username])
+        inserter_username_container.set_topleft((108 * GUI_SCALE, 76 * GUI_SCALE))
+
+        self.inserter_password = gui_utils.create_inserter(res)
+        inserter_password_container = thorpy.Ghost(elements=[self.inserter_password])
+        inserter_password_container.set_topleft((108 * GUI_SCALE, 113 * GUI_SCALE))
+
         login_bg = thorpy.Background()
         login_bg.set_topleft((79 * GUI_SCALE, 55 * GUI_SCALE))
         login_bg.set_size(size=(150 * GUI_SCALE, 121 * GUI_SCALE))
         login_bg.set_image(res.load_login_menu_background())
 
-        self.container = thorpy.Ghost(elements=[button_exit_login,login_bg])
+        self.container = thorpy.Ghost(elements=[login_bg,button_exit_login,button_login,button_register,
+                                                text_username,
+                                                text_password,
+                                                self.text_error,
+                                                inserter_username_container,
+                                                inserter_password_container])
         self.menu = thorpy.Menu(self.container)
         for element in self.menu.get_population():
             element.surface = screen
 
     def exit_login_menu(self):
-        # TODO QUIT GAME HERE
         print("quit game")
         pygame.quit()
+
+    def login(self):
+        username = self.inserter_username.get_value()
+        password = self.inserter_password.get_value()
+        print("login: "+username+" "+password)
+        res = ServerConnector.login(username,password)
+        if res[0] != 200:
+            self.text_error.set_font_color(RED)
+            self.text_error.set_text(res[1])
+        else:
+            self.text_error.set_text("")
+            self.is_opened = False
+            main_menu = MainMenu(self.res, self.screen, self.clock)
+            main_menu.launch()
+
+    def register(self):
+        username = self.inserter_username.get_value()
+        password = self.inserter_password.get_value()
+        print("register: " + username + " " + password)
+        res = ServerConnector.register(username, password)
+        if res[0] != 200:
+            self.text_error.set_font_color(RED)
+            self.text_error.set_text(res[1])
+        else:
+            self.text_error.set_font_color(WHITE)
+            self.text_error.set_text(res[1])
 
     def launch(self):
         while self.is_opened:
