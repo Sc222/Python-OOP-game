e05ad6ce2e1b1dc27dd7d4a09a2f18e3ff6c8620
Sc222 <v.a.shaternikov@gmail.com>
2020-06-27 08:34:25 +0500

    connector update

--- Client/main/server_connector/server_connector.py
+++ Client/main/server_connector/server_connector.py
@@ -3,13 +3,20 @@ import os
 import requests
 from tabulate import tabulate
 
-from main.server_connector.api import Api
-
-
 class ServerConnector:
+    LOCAL_SERVER_LINK = "http://127.0.0.1:5000/"
+    GET_LEADERBOARDS = "leaderboard"
+    GET_USER = "user"
+    SAVE_LEADERBOARDS = "leaderboard/post"
+    REGISTER_USER = "register"
+    LOGIN_USER = "login"
+    SESSION_COOKIE = "session"
+    DATA_FILE = "data.txt"
+    DATA_FILE_LOCATION = "data"
+
 
     @staticmethod
-    def get_leaderboards_formatted(level: int, server_link: str = Api.LOCAL_SERVER_LINK):
+    def get_leaderboards_formatted(level: int, server_link: str = LOCAL_SERVER_LINK):
         leaderboards = ServerConnector.get_leaderboards(level, server_link)
         if len(leaderboards) == 0:
             leaderboards.append(["No records", "at the", "moment"])
@@ -19,53 +26,62 @@ class ServerConnector:
         return formatted_leaderboard
 
     @staticmethod
-    def get_leaderboards(level: int, server_link: str = Api.LOCAL_SERVER_LINK):
-        leaderboards_records = Api.get_leaderboards(level, server_link)
+    def get_leaderboards(level: int, server_link: str = LOCAL_SERVER_LINK):
+        data = {"level": level}
+        r = requests.get(server_link + ServerConnector.GET_LEADERBOARDS, params=data)
+        leaderboards_records = r.json()
         result = []
         for record in leaderboards_records:
             result.append([record["playerName"], record["levelId"], record["score"]])
         return result
 
     @staticmethod
-    def save_leaderboard(login: str, score: int, level: int, cookie: str, server_link: str = Api.LOCAL_SERVER_LINK):
+    def get_user(name: str, server_link: str = LOCAL_SERVER_LINK):
+        cookies = dict(session=ServerConnector.get_cookie())
+        r: requests.Response = requests.get(server_link + ServerConnector.GET_USER+"/"+name,cookies=cookies)
+        print(r.text)
+        return r.json()[0]
+
+    @staticmethod
+    def save_leaderboard(login: str, score: int, level: int, server_link: str = LOCAL_SERVER_LINK):
         data = {"login": login, "score": score, "level": level}
-        cookies = dict(session=cookie)
-        r: requests.Response = requests.post(server_link + Api.SAVE_LEADERBOARDS, params=data, cookies=cookies)
+        cookies = dict(session=ServerConnector.get_cookie())
+        r: requests.Response = requests.post(server_link + ServerConnector.SAVE_LEADERBOARDS, params=data, cookies=cookies)
         result_msg = "status: " + str(r.status_code.real) + " response:" + r.text
         print(result_msg)
         return result_msg
 
     @staticmethod
-    def register(login: str, password: str, server_link: str = Api.LOCAL_SERVER_LINK):
+    def register(login: str, password: str, server_link: str = LOCAL_SERVER_LINK):
         if login == "":
             return 400, "Enter username"
         if password == "":
             return 400, "Enter password"
 
         data = {"login": login}
-        r: requests.Response = requests.post(server_link + Api.REGISTER_USER, params=data, data={"password": password})
+        r: requests.Response = requests.post(server_link + ServerConnector.REGISTER_USER, params=data, data={"password": password})
         result_msg = "status: " + str(r.status_code.real) + " response:" + r.text
         print(result_msg)
         return r.status_code.real, r.text
 
     @staticmethod
-    def login(login: str, password: str, server_link: str = Api.LOCAL_SERVER_LINK):
+    def login(login: str, password: str, server_link: str = LOCAL_SERVER_LINK):
         if login == "":
             return 400, "Enter username"
         if password == "":
             return 400, "Enter password"
         data = {"login": login}
-        r: requests.Response = requests.post(server_link + Api.LOGIN_USER, params=data, data={"password": password})
+        r: requests.Response = requests.post(server_link + ServerConnector.LOGIN_USER, params=data, data={"password": password})
         result_msg = "status: " + str(r.status_code.real) + " response:" + r.text
         print(result_msg)
         if r.status_code.real == 200 and r.text == "Logged in successfully as " + login:
-            ServerConnector.save_data(r.cookies[Api.SESSION_COOKIE], login)
+            ServerConnector.save_data(r.cookies[ServerConnector.SESSION_COOKIE], login)
         return r.status_code.real, r.text
 
     @staticmethod
-    def is_logged_in(server_link: str = Api.LOCAL_SERVER_LINK):
+    def is_logged_in(server_link: str = LOCAL_SERVER_LINK):
         cookies = dict(session=ServerConnector.get_cookie())
-        r: requests.Response = requests.post(server_link + Api.LOGIN_USER, cookies=cookies)
+        r: requests.Response = requests.post(server_link + ServerConnector.LOGIN_USER, cookies=cookies)
         result_msg = "status: " + str(r.status_code.real) + " response:" + r.text
         print(result_msg)
         if r.status_code.real == 200:
@@ -74,7 +90,7 @@ class ServerConnector:
 
     @staticmethod
     def save_data(cookie: str, login: str):
-        f = open(os.path.join(Api.DATA_FILE_LOCATION, Api.DATA_FILE), 'w')
+        f = open(os.path.join(ServerConnector.DATA_FILE_LOCATION, ServerConnector.DATA_FILE), 'w')
         print(cookie + " " + login)
         f.write(cookie)
         f.write("\n" + login)
@@ -83,7 +99,7 @@ class ServerConnector:
 
     @staticmethod
     def get_cookie():
-        path_to_file = os.path.join(Api.DATA_FILE_LOCATION, Api.DATA_FILE)
+        path_to_file = os.path.join(ServerConnector.DATA_FILE_LOCATION, ServerConnector.DATA_FILE)
         if not os.path.isfile(path_to_file):
             f = open(path_to_file, 'w+')
             f.write("\n")
@@ -102,7 +118,7 @@ class ServerConnector:
 
     @staticmethod
     def get_saved_username():
-        path_to_file = os.path.join(Api.DATA_FILE_LOCATION, Api.DATA_FILE)
+        path_to_file = os.path.join(ServerConnector.DATA_FILE_LOCATION, ServerConnector.DATA_FILE)
         if not os.path.isfile(path_to_file):
             f = open(path_to_file, 'w+')
             f.write("\n")
